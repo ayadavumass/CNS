2a845ecc14867128c05767eaa80f0baa6b55c9ff
Aditya Yadav <ayadav@cs.umass.edu>
2017-03-29 16:04:20 -0400

    Changed mysql thread pool from combopooleddatasource to apache dbcp

--- src/edu/umass/cs/contextservice/database/datasource/MySQLDataSource.java
+++ src/edu/umass/cs/contextservice/database/datasource/MySQLDataSource.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import edu.umass.cs.contextservice.config.ContextServiceConfig;
 import edu.umass.cs.contextservice.logging.ContextServiceLogger;
@@ -18,10 +18,12 @@ public class MySQLDataSource extends AbstractDataSource
 {
 	private final HashMap<Integer, SQLNodeInfo> sqlNodeInfoMap;
 	
-    private ComboPooledDataSource cpds;
+    //private ComboPooledDataSource cpds;
+	private BasicDataSource dataSource;
     private final int myNodeId;
 
-    public MySQLDataSource(Integer myNodeID) throws IOException, SQLException, PropertyVetoException 
+    // Commented to test which connection pool is better, apache dbcp or ComboPooledDataSource
+    /*public MySQLDataSource(Integer myNodeID) throws IOException, SQLException, PropertyVetoException 
     {
     	myNodeId = myNodeID;
     	this.sqlNodeInfoMap = new HashMap<Integer, SQLNodeInfo>();
@@ -66,6 +68,8 @@ public class MySQLDataSource extends AbstractDataSource
         // size should be set greater than that. These things affect system performance a lot.
         cpds.setMinPoolSize(ContextServiceConfig.MYSQL_MAX_CONNECTIONS);
         cpds.setMaxPoolSize(ContextServiceConfig.MYSQL_MAX_CONNECTIONS);
+        System.out.println("Max idle time before "+cpds.getMaxIdleTime());
+        
         cpds.setMaxIdleTime(0);
         
         cpds.setAutoCommitOnClose(false);
@@ -73,13 +77,65 @@ public class MySQLDataSource extends AbstractDataSource
         //cpds.setMaxStatements(180);
         ContextServiceLogger.getLogger().fine("HyperspaceMySQLDB datasource "
         		+ "max pool size "+cpds.getMaxPoolSize());
+    }*/
+    
+    
+    public MySQLDataSource(Integer myNodeID) 
+    			throws IOException, SQLException, PropertyVetoException 
+    {
+    	myNodeId = myNodeID;
+    	this.sqlNodeInfoMap = new HashMap<Integer, SQLNodeInfo>();
+    	readDBNodeSetup();
+    	
+    	int portNum = sqlNodeInfoMap.get(myNodeID).portNum;
+    	String dbName = sqlNodeInfoMap.get(myNodeID).databaseName;
+    	String username = sqlNodeInfoMap.get(myNodeID).username;
+    	String password = sqlNodeInfoMap.get(myNodeID).password;
+    	String arguments = sqlNodeInfoMap.get(myNodeID).arguments;
+    	
+    	dropDB(sqlNodeInfoMap.get(myNodeID));
+    	createDB(sqlNodeInfoMap.get(myNodeID));
+    	
+    	dataSource = new BasicDataSource();
+        //cpds = new ComboPooledDataSource();
+    	//dataSource.setDriverClass("com.mysql.jdbc.Driver"); //loads the jdbc driver
+        if(arguments.length() > 0)
+        {
+        	dataSource.setUrl("jdbc:mysql://localhost:"+portNum+"/"+dbName+"?"+arguments);
+        }
+        else
+        {
+        	dataSource.setUrl("jdbc:mysql://localhost:"+portNum+"/"+dbName);
+        }
+        
+        
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        //the settings below are optional -- c3p0 can work with defaults
+        //cpds.setMinPoolSize(5);
+        //cpds.setAcquireIncrement(5);
+        // NOTE: max pool size of DB affects the performance alot
+        // definitely set it, don't leave it for default. default gives very bad
+        // update performance. 
+        // TODO: need to find its optimal value.
+        // on d710 cluster 150 gives the best performance, after that performance remains same.
+        // should be at least same as the hyperspace hashing pool size.
+        // actually default mysql server max connection is 151. So this should be
+        // set in conjuction with that. and also the hyperpsace hashing thread pool
+        // size should be set greater than that. These things affect system performance a lot.
+       
+        dataSource.setMaxTotal(ContextServiceConfig.MYSQL_MAX_CONNECTIONS);
+        dataSource.setMaxIdle(ContextServiceConfig.MYSQL_MAX_CONNECTIONS);
+        
+        dataSource.setEnableAutoCommitOnReturn(false);
     }
     
 
     public Connection getConnection(DB_REQUEST_TYPE dbReqType) throws SQLException 
     {
     	long start = System.currentTimeMillis();
-    	Connection conn = this.cpds.getConnection();
+    	Connection conn = this.dataSource.getConnection();
     	if(ContextServiceConfig.DEBUG_MODE)
     	{
     		System.out.println("TIME_DEBUG getConnection "
